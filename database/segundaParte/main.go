35ff1b297fa41b4cf354f1bb07567cb2999ee1bb
leonardogregoriocs <leonardogregoriocs@gmail.com>
2023-09-14 14:19:19 -0300

    Studies Go - Database

---
+++ database/segundaParte/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID    int `gorm:"primaryKey"`
+	Name  string
+	Price float64
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{
+		Name:  "Notebook",
+		Price: 1000.0,
+	})
+
+	// Create batch
+	products := []Product{
+		{Name: "PS5", Price: 5000},
+		{Name: "PS4", Price: 2000},
+		{Name: "Camisete", Price: 50.00},
+	}
+	db.Create(&products)
+
+}
