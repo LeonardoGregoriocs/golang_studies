fe8ae55d2e14b93900c09201e12c6903fdcc5b2f
leonardogregoriocs <leonardogregoriocs@gmail.com>
2023-09-16 14:12:49 -0300

    Studies Go - Database

--- database/segundaParte/main.go
+++ database/segundaParte/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -20,17 +22,36 @@ func main() {
 	db.AutoMigrate(&Product{})
 
 	// Create
-	db.Create(&Product{
-		Name:  "Notebook",
-		Price: 1000.0,
-	})
+	// db.Create(&Product{
+	// 	Name:  "Notebook",
+	// 	Price: 1000.0,
+	// })
 
 	// Create batch
-	products := []Product{
-		{Name: "PS5", Price: 5000},
-		{Name: "PS4", Price: 2000},
-		{Name: "Camisete", Price: 50.00},
+	// products := []Product{
+	// 	{Name: "PS5", Price: 5000},
+	// 	{Name: "PS4", Price: 2000},
+	// 	{Name: "Camisete", Price: 50.00},
+	// }
+	// db.Create(&products)
+
+	// Select one
+	var product, productTwo Product
+	fmt.Printf("Select One with filter id\n")
+	db.First(&product, 1)
+	fmt.Println(product)
+
+	fmt.Printf("\nSelect One with filter name\n")
+	db.First(&productTwo, "name = ?", "PS5")
+	fmt.Println(productTwo)
+
+	// Select All
+	var products []Product
+	db.Find(&products)
+	fmt.Printf("\nSelect All\n")
+	for _, product := range products {
+		fmt.Println(product)
+
 	}
-	db.Create(&products)
 
 }
