5a8331a1a532f08faa52ee3438758c4853f35455
leonardogregoriocs <leonardogregoriocs@gmail.com>
2023-09-26 10:52:17 -0300

    Studies Go - Packging

--- packging/primeiraParte/cmd/main.go
+++ packging/primeiraParte/cmd/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/leonardogregoriocs/golang_studies/packging/primeiraParte/math"
 )
 
 func main() {
+	m := math.NewMath(1, 3)
+	fmt.Println(m.Add())
 	fmt.Println("Hello, world")
 }
