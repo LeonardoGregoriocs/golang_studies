d546901150d2faeea1f2fac7327239fc658884a9
leonardogregoriocs <leonardogregoriocs@gmail.com>
2023-09-28 11:26:20 -0300

    Studies Go - Testing

--- testing/segundaParte/tax_test.go
+++ testing/segundaParte/tax_test.go
@@ -1,10 +1,21 @@
 package tax
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
+
+type TaxRepositoryMock struct {
+	mock.Mock
+}
+
+func (m *TaxRepositoryMock) SaveTax(tax float64) error {
+	args := m.Called(tax)
+	return args.Error(0)
+}
 
 func TestCalculateTax(t *testing.T) {
 	tax, err := CalculaTax(1000.0)
@@ -14,4 +25,19 @@ func TestCalculateTax(t *testing.T) {
 	tax, err = CalculaTax(0)
 	assert.Error(t, err, "amout must be greater than 0")
 	assert.Equal(t, 0.0, tax)
+}
+
+func TestCalculateTaxAndSave(t *testing.T) {
+	repository := &TaxRepositoryMock{}
+	repository.On("SaveTax", 10.0).Return(nil)
+	repository.On("SaveTax", 0.0).Return(errors.New("error saving tax"))
+
+	err := CalculateTaxAndSave(1000.0, repository)
+	assert.Nil(t, err)
+
+	err = CalculateTaxAndSave(0, repository)
+	assert.Error(t, err, "error saving tax")
+
+	repository.AssertExpectations(t)
+	repository.AssertNumberOfCalls(t, "SaveTax", 2)
 }
