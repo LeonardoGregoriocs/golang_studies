0b13643c176489555c3c2ba2c7233e7ae73b39cb
leonardogregoriocs <leonardogregoriocs@gmail.com>
2024-01-10 16:34:26 -0300

    comment line go private

--- go_private/main.go
+++ go_private/main.go
@@ -1,15 +1,9 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/leonardogregoriocs/fcutils-secret/pkg/events"
-)
-
-func main() {
-	ed := events.NewEventDispatcher()
-	fmt.Println(ed)
-}
+// func main() {
+// 	ed := events.NewEventDispatcher()
+// 	fmt.Println(ed)
+// }
 
 // Como fazer com que o Go baixe informações de repositório privado?
 // Alteramos a váriavel de ambiente GOPRIVATE
